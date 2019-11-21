16da90529a9a605809af77957d0ac7514a3e4402
limeb01 <emiboi2001@gmail.com>
2019-11-21 22:18:37 +0100

    Update Main.java
    miniräknare

--- Main.java
+++ Main.java
@@ -1,159 +1,176 @@
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
-import javafx.scene.control.*;
-import javafx.scene.shape.Rectangle;
-import javafx.event.ActionEvent;
-
-import java.awt.*;
 import javafx.scene.control.Button;
 import java.util.ArrayList;
 
-public class layout extends Application {
-HBox hbox = new HBox();
+public class *Main.java extends Application {
+    private String tal="";
+    private HBox hbox = new HBox();
+    private String textfieldText = "";
+    private BorderPane miniraknare = new BorderPane();
+    private TextField textField = new TextField();
+    private ArrayList<Button> KnapparArray = new ArrayList<Button>();
 
-String nummer = "";
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-BorderPane miniräknare = new BorderPane();
+    @Override
+    public void start(Stage primaryStage) {
+        knappar();
 
-ArrayList<Button> KnapparArray = new ArrayList<Button>();
+        miniraknare.setPrefHeight(600);
+        miniraknare.setPrefWidth(400);
+        miniraknare.setCenter(borderpaneCenter());
+        miniraknare.setTop(hbox);
 
-public static void main(String[] args) {
-launch(args);
-}
+        textfieldMetod("");
 
+        Scene calculatorScene = new Scene(miniraknare);
 
-@Override
-public void start(Stage primaryStage) {
-knappar();
+        primaryStage.setTitle("Calculator");
+        primaryStage.setScene(calculatorScene);
+        primaryStage.show();
+    }
 
-miniräknare.setPrefHeight(600);
-miniräknare.setPrefWidth(400);
+    private StackPane borderpaneCenter() {
+        StackPane center = new StackPane();
+        GridPane buttons = new GridPane();
+        buttons.setHgap(10);
+        buttons.setVgap(10);
+        int rad = 1;
+        int kolumner = 1;
+        for (int i = 0; i < KnapparArray.size(); i++) {
+            if (i % 4 == 0) {
+                rad++;
+                kolumner = 1;
+            }
+            buttons.add(KnapparArray.get(i), kolumner, rad);
+            kolumner++;
+        }
+        center.getChildren().add(buttons);
 
+        return center;
+    }
 
-miniräknare.setCenter(borderpaneCenter());
+    private void textfieldMetod(String input) {
+        if(input.contains("=")){
+            textfieldText="";
+            try{
+                uträkning();
+            }
+            catch (Exception e){
+            System.out.println("ERROR");
+            }
 
-miniräknare.setTop(hbox);
-textfield("t");
+        } else {
+            textfieldText = textfieldText + input;
+        }
+        if (input.contains("C")){
+            textfieldText="";
+        }
 
-Scene calculatorScene = new Scene(miniräknare);
-
-primaryStage.setTitle("Calculator");
-primaryStage.setScene(calculatorScene);
-primaryStage.show();
-
-}
-
-
-public StackPane borderpaneCenter() {
-
-
-StackPane center = new StackPane();
-GridPane buttons = new GridPane();
-buttons.setHgap(10);
-buttons.setVgap(10);
-int rad = 1;
-int kolumner = 1;
-for (int i = 0; i < KnapparArray.size(); i++) {
-
-
-if (i % 4 == 0) {
-rad++;
-kolumner = 1;
-}
-
-
-buttons.add(KnapparArray.get(i), kolumner, rad);
-kolumner++;
-}
-
-center.getChildren().add(buttons);
-
-return center;
-}
-public void textfield(String input){
-TextField textField = new TextField(input);
-textField.setPrefHeight(100);
-textField.setPrefWidth(500);
-textField.setStyle(("-fx-font: 45 arial;"));
-
-hbox.getChildren().setAll(textField);
-
-}
-
-public void knappar() {
-
-char[] nummer = {'7', '8', '9', 'x', '4', '5', '6', '÷', '1', '2', '3', '-', '.', '0', '=', '+', 'C', 'D'};
-int count = 0;
-
-for (char t : nummer) {
-
-char charr = nummer[count];
-System.out.println(charr);
-String knappString = String.valueOf(charr);
-Button knapp = new Button(knappString);
-knapp.setStyle("-fx-font: 20 arial;");
-knapp.setOnAction(event -> {
-uträkning(knappString);
-System.out.println("hej");
-});
-knapp.setPrefHeight(75);
-knapp.setPrefWidth(75);
-KnapparArray.add(knapp);
-count++;
-}
+        textField = new TextField(textfieldText);
+        textField.setPrefHeight(100);
+        textField.setPrefWidth(500);
+        textField.setStyle(("-fx-font: 45 arial;"));
+        hbox.getChildren().setAll(textField);
 
 
-}
-public void uträkning(String input){
-double result = 0;
-System.out.println(input);
-ArrayList <Double> talArray = new ArrayList<>();
+    }
 
-nummer = nummer + input;
-if(input.charAt(0)=='C') {
-nummer = "";
+    private void knappar() {
 
-}
-if(input.charAt(0)=='=') {
+        char[] nummer = {'7', '8', '9', '*', '4', '5', '6', '÷', '1', '2', '3', '-', '=', '0', 'C', '+'};
+        int count = 0;
 
-for (int i = 0; i < nummer.length(); i++) {
-char temp = nummer.charAt(i);
-String tal = Character.toString(temp);
-if (Character.isDigit(temp) == false) {
-talArray.add(Double.parseDouble(tal));
+        for (char t : nummer) {
+            char charr = nummer[count];
+            System.out.println(charr);
+            String knappString = String.valueOf(charr);
+            Button knapp = new Button(knappString);
+            knapp.setStyle("-fx-font: 20 arial;");
+            knapp.setOnAction(event -> {
+                textfieldMetod(knappString);
+            });
+            knapp.setPrefHeight(75);
+            knapp.setPrefWidth(75);
+            KnapparArray.add(knapp);
+            count++;
+        }
+    }
 
-tal = "";
 
-}
-}
+    private void uträkning(){
+        String input = textField.getText();
+        ArrayList <Double> numbers =  splitring(input);
+        System.out.println(input);
 
-for (int i = 0; i < nummer.length(); i++) {
-if (Character.isDigit(nummer.charAt(i))) {
+        for (int i = 0; i < input.length(); i++) {
+            if (Character.isDigit(input.charAt(i))==false){
+                if (input.charAt(i)=='+'){
+                    double resultat = numbers.get(0) + numbers.get(1);
+                    numbers.remove(1);
+                    numbers.remove(0);
+                    numbers.add(0,resultat);
+                }
+                if(input.charAt(i)=='-'){
+                    double resultat = numbers.get(0) - numbers.get(1);
+                    numbers.remove(1);
+                    numbers.remove(0);
+                    numbers.add(0,resultat);
+                }
+                if (input.charAt(i)=='*'){
+                    double resultat = numbers.get(0) * numbers.get(1);
+                    numbers.remove(1);
+                    numbers.remove(0);
+                    numbers.add(0,resultat);
 
-if (nummer.charAt(i) == '+') {
-result = talArray.get(0) + talArray.get(1);
+                }
+                if (input.charAt(i)=='÷'){
+                    double resultat = numbers.get(0) / numbers.get(1);
+                    numbers.remove(1);
+                    numbers.remove(0);
+                    numbers.add(0,resultat);
 
-}
+                }
+            }
+        }
+        double temp = numbers.get(0);
+        int resultatSomInt = (int) temp;
+        String resultatString = Integer.toString(resultatSomInt);
 
-}
-}
+        textfieldMetod(resultatString);
+    }
 
-String resultString = Double.toString(result);
+    // delar up de olika termerna
+    private ArrayList<Double> splitring(String input){
+        tal = "";
+        System.out.println(input+" INPUT");
+        ArrayList <Double> talArray = new ArrayList<>();
+        double talDouble =0;
+        for (int i = 0; i < input.length(); i++) {
+            if (Character.isDigit(input.charAt(i))==false){
+                talDouble = Double.parseDouble(tal);
+                talArray.add(talDouble);
+                tal = "";
+            }
+            else{
+                tal = tal + input.charAt(i);
+            }
+        }
+        if (tal.length()>=1) {
+            talDouble = Double.parseDouble(tal);
+            talArray.add(talDouble);
+        }
 
-textfield(resultString);
-}
-else{
-textfield(nummer);
-}
-
-}
-
+        System.out.println(talArray);
+        return talArray;
+    }
 }
